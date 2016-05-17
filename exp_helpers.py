7698b29a73794869b1eea920c5613f279e0fa4b5
BethanyL <herwaldt@uw.edu>
2016-05-17 13:09:09 -0700

    further refactoring
    another working state

--- exp_helpers.py
+++ exp_helpers.py
@@ -155,3 +155,82 @@ def initialize_new_file(header_string, trial_counter):
 # Returns actual image labels
 def get_actual_image_labels(sess, actual, y_, test_labels):
     return sess.run(actual, feed_dict={y_: test_labels})
+
+# Assembles datasets    
+def prepare_data():
+    # Create TensorFlow data objects (contains all images and labels)
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    
+    ############
+    # Even out class sizes, 892 images each, limited by the smallest class of only 892 images.
+    test_images = []
+    test_labels = []
+    counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    magic_val = 852
+    count = 0
+    for i in range(len(mnist.test.labels)):
+        index = np.nonzero(mnist.test.labels[i])[0][0]
+        if counts[index] < magic_val:
+            test_images.append(mnist.test.images[i])
+            test_labels.append(mnist.test.labels[i])
+            count = count + 1
+        counts[index] = counts[index] + 1
+
+    # New evenly sized test sets.
+    test_images = np.asarray(test_images)
+    test_labels = np.asarray(test_labels)
+    
+    return [test_images, test_labels]
+    
+def setup_network():
+    sess = tf.InteractiveSession()
+    
+    ############
+    # TensorFlow setup
+    x = tf.placeholder("float", shape=[None, 784])
+    y_ = tf.placeholder("float", shape=[None, 10])
+
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([7 * 7 * 64, 10])
+    b_fc1 = bias_variable([10])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    keep_prob = tf.placeholder("float")
+    y_conv = tf.nn.softmax(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    predicted = tf.argmax(y_conv, 1)
+    actual = tf.argmax(y_, 1)
+    correct_prediction = tf.equal(predicted, actual)
+
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    sess.run(tf.initialize_all_variables())
+
+    ############
+    # import already trained model/network from file.
+    saver = tf.train.Saver()
+    ckpt = tf.train.get_checkpoint_state(os.getcwd())
+    saver.restore(sess, ckpt.model_checkpoint_path)
+
+    ############
+    # Convert weight matrices from tensorflow tensor objects to real value numpy arrays with sess.run()
+    # packs them together in a list to contain all of the weight matrices.
+    matrices_to_damage =\
+        [np.asarray(sess.run(W_conv1)),
+         np.asarray(sess.run(W_conv2)),
+         np.asarray(sess.run(W_fc1))]
+         
+    return [sess, actual, y_, y_conv, x, keep_prob, W_conv1, W_conv2, W_fc1, matrices_to_damage]
